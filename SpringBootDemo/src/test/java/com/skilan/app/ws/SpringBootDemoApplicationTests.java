4009c570ecda57fa4205ed1dd5d019e51cf5b163
Senthil Subramanian <27633468+SenthilSubramanian@users.noreply.github.com>
2020-04-25 12:03:04 +0530

    SpringBoot - Initial commit

---
+++ SpringBootDemo/src/test/java/com/skilan/app/ws/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilan.app.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
